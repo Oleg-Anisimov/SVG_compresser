d983e5f7a5e83ad8aabfbb7f1f4d47d088c69fd1
Lemain99 <102553300+Lemain99@users.noreply.github.com>
2022-08-23 22:41:08 +0300

    Initial commit

---
+++ src/Compressor.java
@@ -0,0 +1,56 @@
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+class Compressor {
+    public static int compress(String... args) throws IOException {
+
+        if (args.length < 1 || isNullOrWhitespace(args[0])) {
+            System.out.println("Run app with argument: path to svg");
+            return 1;
+        }
+
+
+        for (String pathFile : args) {
+
+            File file = new File(pathFile);
+            if (!file.exists()) {
+                System.out.println("No such file: \"{args[0]}\"");
+                return 2;
+            }
+            String newFilePath = FilenameUtils.getFullPath(pathFile)+"\\"+"Compressed .SVG";
+
+            String match = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+            String newFileText = match.replaceAll("(\\d+\\.\\d)(?:\\d+)", "$1");
+
+
+            /// Круто было бы иметь возможность включать это опционально
+            if (!newFileText.contains("<?xml version=\"1.0\" encoding=\"utf-8\"?>")) {
+                newFileText = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + newFileText;
+            }
+
+            Files.write(Path.of(newFilePath), newFileText.getBytes(StandardCharsets.UTF_8));
+
+        }
+        System.out.println("Done!");
+        return 0;
+    }
+
+    public static boolean isNullOrWhitespace(String s) {
+        if (s == null)
+            return true;
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isWhitespace(s.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
---
+++ src/FileChooser.java
@@ -0,0 +1,87 @@
+import javax.swing.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class FileChooser extends JFrame {
+    private JButton btnOpenDir = null;
+    private JFileChooser fileChooser = null;
+
+    public FileChooser() {
+        super("FileChooser");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        btnOpenDir = new JButton("Открыть директорию");
+
+        fileChooser = new JFileChooser();
+
+
+        JPanel contents = new JPanel();
+        JTextArea myPanel = new JTextArea(10,50);
+
+        contents.add(btnOpenDir);
+
+        contents.add(myPanel);
+        setContentPane(contents);
+
+        setSize(500, 300);
+        setVisible(true);
+
+        btnOpenDir.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                fileChooser.setDialogTitle("Выбор директории");
+                // Определение режима - только каталог
+                fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                int result = fileChooser.showOpenDialog(FileChooser.this);
+                var path = fileChooser.getSelectedFile();
+                File file = new File(path.toURI());
+                // Если директория выбрана, покажем ее в сообщении
+                if (result == JFileChooser.APPROVE_OPTION){
+                    try {
+                        Compressor.compress(file.getAbsolutePath());
+                    } catch (IOException ex) {
+                        JOptionPane.showMessageDialog(FileChooser.this,
+                                fileChooser.getSelectedFile());
+                        throw new RuntimeException(ex);
+
+                    }
+                }
+
+            }
+        });
+
+        myPanel.setDropTarget(new DropTarget() {
+            public synchronized void drop(DropTargetDropEvent evt) {
+                try {
+                    evt.acceptDrop(DnDConstants.ACTION_COPY);
+                    List<File> droppedFiles = (List<File>)
+                            evt.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);
+                    for (File file : droppedFiles) {
+                        try {
+                            Compressor.compress(file.getAbsolutePath());
+                        } catch (IOException ex) {
+                            JOptionPane.showMessageDialog(FileChooser.this,
+                                    fileChooser.getSelectedFile());
+                            throw new RuntimeException(ex);
+
+                        }
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+
+}
+
+
+
